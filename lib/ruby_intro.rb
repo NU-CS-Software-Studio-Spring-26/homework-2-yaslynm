1cfb02aa120939943145ef5f4c58014ce1e66bc9
yaslynm <152467060+yaslynm@users.noreply.github.com>
2026-04-22 02:53:55 -0500

    completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,51 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr[0] if arr.length == 1
+  return 0 if arr.empty?
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  seen = {}
+  arr.each do |num|
+    return true if seen[n - num]
+    seen[num] = true
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s =~ /\A[bcdfghjklmnpqrstvwxyz]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s.match?(/\A[01]+\z/)
+  s == "0" || s.end_with?("00")
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
